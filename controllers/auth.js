433a75e5971e86dfd6d56d4a44cfddf2b6df7979
dlswilliam <santo@Project2.localdomain>
2026-01-22 00:49:14 -0500

    stuck on the favorite button but added the ability to edit a hunt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,80 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const User = require('../models/user.js');
-
-router.get('/sign-up', (req, res) => {
-  res.render('auth/sign-up.ejs');
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
 });
 
-router.get('/sign-in', (req, res) => {
-  res.render('auth/sign-in.ejs');
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
 });
 
-router.get('/sign-out', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+router.get("/sign-out", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 });
 
-router.post('/sign-up', async (req, res) => {
-  try {
-    // Check if the username is already taken
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-      return res.send('Username already taken.');
-    }
-  
-    // Username is not taken already!
-    // Check if the password and confirm password match
-    if (req.body.password !== req.body.confirmPassword) {
-      return res.send('Password and Confirm Password must match');
-    }
-  
-    // Must hash the password before sending to the database
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    req.body.password = hashedPassword;
-  
-    // All ready to create the new user!
-    await User.create(req.body);
-  
-    res.redirect('/auth/sign-in');
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+router.post("/sign-in", async (req, res) => {
+  const userInDB = await User.findOne({ username: req.body.username }).select('+password');
 
-router.post('/sign-in', async (req, res) => {
-  try {
-    // First, get the user from the database
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (!userInDatabase) {
-      return res.send('Login failed. Please try again.');
-    }
-  
-    // There is a user! Time to test their password with bcrypt
-    const validPassword = bcrypt.compareSync(
-      req.body.password,
-      userInDatabase.password
+  if (!userInDB) {
+    return res.send(
+      `A user with username ${req.body.username} does not exist.`
     );
-    if (!validPassword) {
-      return res.send('Login failed. Please try again.');
-    }
-  
-    // There is a user AND they had the correct password. Time to make a session!
-    // Avoid storing the password, even in hashed format, in the session
-    // If there is other data you want to save to `req.session.user`, do so here!
-    req.session.user = {
-      username: userInDatabase.username,
-      _id: userInDatabase._id
-    };
-  
-    res.redirect('/');
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
   }
+
+  const isValidPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDB.password
+  );
+  if (!isValidPassword) {
+    return res.send(`Password Incorrect`);
+  }
+
+  req.session.user = {
+    username: userInDB.username,
+    _id: userInDB._id,
+  };
+
+  req.session.save(() => {
+    res.redirect("/");
+  });
+});
+
+router.post("/sign-up", async (req, res) => {
+  const userInDB = await User.findOne({ username: req.body.username });
+  if (userInDB) {
+    return res.send(`A user with username ${req.body.username} already exist.`);
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send(`Your password and password confirm must match!`);
+  }
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  const newUser = await User.create(req.body);
+
+  req.session.user = {
+    username: newUser.username,
+    _id: newUser._id,
+  };
+
+  req.session.message = `Welcome to Monster Hunter ${newUser.username}`
+
+  req.session.save(() => {
+    res.redirect("/hunts");
+  });
 });
 
 module.exports = router;
